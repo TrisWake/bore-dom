6f08d47f1e1e1527222781e2326ab84256929f5e
Tristan Wakefield <tristan.wakefield@yahoo.com>
2024-10-31 10:33:14 -0500

    initial

---
+++ main.js
@@ -0,0 +1,29 @@
+// 1. Alter the text color of the paragraph to a shade of aqua.
+const paragragh = document.querySelector('p')
+paragragh.style.color = "aqua"
+
+
+// 2. Adjust the font size of the heading to 42 pixels.
+const heading = document.querySelector('h1')
+heading.style.fontSize = '42px'
+
+// 3. Replace the text in the paragraph with the new Lorem Ipsum content.
+paragragh.innerText = "lorem Ipsum"
+// 4. Reduce the opacity of the 13th item to 30% transparency.
+const list = document.querySelectorAll('ul')
+list[13].style.opacity = "30%"
+// 5. Modify the text of the 3rd item to read "Greetings!"
+list[3].innerText = "Greetings!"
+// 6. Update the image below the list to display the new image.
+Image.src = "bore-dom\meme-duck-i-cant-believe-you-6ot-plastic-surgery.jpeg"
+// 7. Resize the new image to a height of 250 pixels.
+Image.style.height = "250px"
+// 8. Integrate an additional image using innerHTML below the existing image.
+Image.innerHTML =+ "https://www.nylabone.com/-/media/project/oneweb/nylabone/images/dog101/10-intelligent-dog-breeds/golden-retriever-tongue-out.jpg"
+// 9. Adjust the height of the new image to 280 pixels programmatically.
+image[2].style.height = "280px"
+// 10. Append a 16th <li> element to the list using innerHTML.
+list[16].innerHTML = "<li>16</li>
+// 11. Assign the same class as the other list items to the new <li> element using JavaScript.
+
+// 12. Populate the new <li> with the text "Never gonna give you up."
